5d8d9537e3b59d7bc5a082778f3199a070e9ab9c
souzaiarley <souzaiarley20@gmail.com>
2025-01-10 16:48:32 -0300

    experiment 0 implemented

--- experiments.py
+++ experiments.py
@@ -12,18 +12,94 @@ def printResult(start: Node, goal: Node, result):
             Solution found:
             Start: {start}
             goal: {goal}
-            Path: {' -> '.join([str(node) for node in reversed(result[0].path())])}
+            Path: {" -> ".join([str(node) for node in reversed(result[0].path())])}
             Path cost: {result[0].cost}
             Nodes generated: {result[1]}
             Nodes visited: {result[2]}
     """
-    
+
+def Experiment_0():
+    with open("experiments/experiment_0.txt", "w") as f:
+        f.write("|==============|\n")
+        f.write("| Experiment 0 |\n")
+        f.write("|==============|\n")
+
+        startX = int(input("\nEnter the x value for the start node: "))
+        startY = int(input("Enter the y value for the start node: "))
+        goalX = int(input("Enter the x value for the goal node: "))
+        goalY = int(input("Enter the y value for the goal node: "))
+
+        start = Node(startX, startY)
+        goal = Node(goalX, goalY)
+
+        f.write(f"\nNodes: Start {start} - Goal {goal}\n")
+            
+        # Breadth-First Search
+        f.write("\n    Breadth-First Search\n")
+
+        # Calling the BFS function with the cost functions
+        f.write("\n        Cost function: C_1")
+        result = BFS(start, goal, C_1)
+        f.write(printResult(start, goal, result))
+
+        f.write("\n        Cost function: C_2")
+        result = BFS(start, goal, C_2)
+        f.write(printResult(start, goal, result))
+
+        f.write("\n        Cost function: C_3")
+        result = BFS(start, goal, C_3)
+        f.write(printResult(start, goal, result))
+
+        f.write("\n        Cost function: C_4")
+        result = BFS(start, goal, C_4)
+        f.write(printResult(start, goal, result))
+
+        # Depth-First Search
+        f.write("\n    Depth-First Search\n")
+
+        # Calling the DFS function with the cost functions
+        f.write("\n        Cost function: C_1")
+        result = DFS(start, goal, C_1)
+        f.write(printResult(start, goal, result))
+
+        f.write("\n        Cost function: C_2")
+        result = DFS(start, goal, C_2)
+        f.write(printResult(start, goal, result))
+
+        f.write("\n        Cost function: C_3")
+        result = DFS(start, goal, C_3)
+        f.write(printResult(start, goal, result))
+
+        f.write("\n        Cost function: C_4")
+        result = DFS(start, goal, C_4)
+        f.write(printResult(start, goal, result))
+
+        # Uniform-Cost Search
+        f.write("\n    Uniform-Cost Search (Dijkstra)\n")
+
+        # Calling the Dijkstra function with the cost functions
+        f.write("\n        Cost function: C_1")
+        result = Dijkstra(start, goal, C_1)
+        f.write(printResult(start, goal, result))
+
+        f.write("\n        Cost function: C_2")
+        result = Dijkstra(start, goal, C_2)
+        f.write(printResult(start, goal, result))
+
+        f.write("\n        Cost function: C_3")
+        result = Dijkstra(start, goal, C_3)
+        f.write(printResult(start, goal, result))
+
+        f.write("\n        Cost function: C_4")
+        result = Dijkstra(start, goal, C_4)
+        f.write(printResult(start, goal, result))
+
 # This function runs the experiment 1, saving the results in a file
 def Experiment_1():
-    with open('experiments/experiment_1.txt', 'w') as f:
-        f.write('|==============|\n')
-        f.write('| Experiment 1 |\n')
-        f.write('|==============|\n')
+    with open("experiments/experiment_1.txt", "w") as f:
+        f.write("|==============|\n")
+        f.write("| Experiment 1 |\n")
+        f.write("|==============|\n")
         
         for i in range(50):
             # Generate random start and goal nodes
@@ -35,64 +111,64 @@ def Experiment_1():
             start = Node(startX, startY)
             goal = Node(goalX, goalY)
 
-            f.write(f'\nNodes: Start {start} - Goal {goal}\n')
+            f.write(f"\nNodes: Start {start} - Goal {goal}\n")
             
             # Breadth-First Search
-            f.write('\n    Breadth-First Search\n')
+            f.write("\n    Breadth-First Search\n")
 
             # Calling the BFS function with the cost functions
-            f.write('\n        Cost function: C_1')
+            f.write("\n        Cost function: C_1")
             result = BFS(start, goal, C_1)
             f.write(printResult(start, goal, result))
 
-            f.write('\n        Cost function: C_2')
+            f.write("\n        Cost function: C_2")
             result = BFS(start, goal, C_2)
             f.write(printResult(start, goal, result))
 
-            f.write('\n        Cost function: C_3')
+            f.write("\n        Cost function: C_3")
             result = BFS(start, goal, C_3)
             f.write(printResult(start, goal, result))
 
-            f.write('\n        Cost function: C_4')
+            f.write("\n        Cost function: C_4")
             result = BFS(start, goal, C_4)
             f.write(printResult(start, goal, result))
 
             # Depth-First Search
-            f.write('\n    Depth-First Search\n')
+            f.write("\n    Depth-First Search\n")
 
             # Calling the DFS function with the cost functions
-            f.write('\n        Cost function: C_1')
+            f.write("\n        Cost function: C_1")
             result = DFS(start, goal, C_1)
             f.write(printResult(start, goal, result))
 
-            f.write('\n        Cost function: C_2')
+            f.write("\n        Cost function: C_2")
             result = DFS(start, goal, C_2)
             f.write(printResult(start, goal, result))
 
-            f.write('\n        Cost function: C_3')
+            f.write("\n        Cost function: C_3")
             result = DFS(start, goal, C_3)
             f.write(printResult(start, goal, result))
 
-            f.write('\n        Cost function: C_4')
+            f.write("\n        Cost function: C_4")
             result = DFS(start, goal, C_4)
             f.write(printResult(start, goal, result))
 
             # Uniform-Cost Search
-            f.write('\n    Uniform-Cost Search (Dijkstra)\n')
+            f.write("\n    Uniform-Cost Search (Dijkstra)\n")
 
             # Calling the Dijkstra function with the cost functions
-            f.write('\n        Cost function: C_1')
+            f.write("\n        Cost function: C_1")
             result = Dijkstra(start, goal, C_1)
             f.write(printResult(start, goal, result))
 
-            f.write('\n        Cost function: C_2')
+            f.write("\n        Cost function: C_2")
             result = Dijkstra(start, goal, C_2)
             f.write(printResult(start, goal, result))
 
-            f.write('\n        Cost function: C_3')
+            f.write("\n        Cost function: C_3")
             result = Dijkstra(start, goal, C_3)
             f.write(printResult(start, goal, result))
 
-            f.write('\n        Cost function: C_4')
+            f.write("\n        Cost function: C_4")
             result = Dijkstra(start, goal, C_4)
             f.write(printResult(start, goal, result))
--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-from experiments import Experiment_1
+from experiments import *
 
 if __name__ == "__main__":
-    Experiment_1()
+    print("Choose an experiment to run:")
+    print("[0] Experiment 0")
+    print("[1] Experiment 1")
+    choice = input("Choice: ")
+
+    if choice == "0":
+        Experiment_0()
+    elif choice == "1":
+        Experiment_1()
+    else:
+        print("Invalid choice.")
