6863a9f7a5c7fe59e2f60d3faf8c4c101160b414
Iarley Souza <souzaiarley20@gmail.com>
2025-01-15 13:52:07 -0300

    reached to generated

--- algorithms.py
+++ algorithms.py
@@ -17,9 +17,9 @@ def BFS(start: Node, goal: Node, costFunction: Callable):
     frontier = Queue()
     frontier.put(currentNode)
 
-    # Create a set to store the nodes that have been reached and add the start node to the set
-    reached = set()
-    reached.add(currentNode)
+    # Create a set to store the nodes that have been generated and add the start node to the set
+    generated = set()
+    generated.add(currentNode)
 
     # Create a list to store the nodes that have been visited
     visited = []
@@ -33,12 +33,12 @@ def BFS(start: Node, goal: Node, costFunction: Callable):
         # Expand the current node
         children = currentNode.expand(costFunction)
 
-        # For each child node, if the child node is the goal node, return the child node. Otherwise, add the child node to the queue and to the reached set
+        # For each child node, if the child node is the goal node, return the child node. Otherwise, add the child node to the queue and to the generated set
         for child in children:
             if child == goal:
-                return (child, len(reached)+1, len(visited)) # Return the goal node, the number of nodes reached (generated), and the number of nodes visited
-            if child not in reached:
-                reached.add(child)
+                return (child, len(generated)+1, len(visited)) # Return the goal node, the number of nodes generated (generated), and the number of nodes visited
+            if child not in generated:
+                generated.add(child)
                 frontier.put(child)
     
     # If the goal node is not found, return None
@@ -57,9 +57,9 @@ def RandomBFS(start: Node, goal: Node, costFunction: Callable):
     frontier = Queue()
     frontier.put(currentNode)
 
-    # Create a set to store the nodes that have been reached and add the start node to the set
-    reached = set()
-    reached.add(currentNode)
+    # Create a set to store the nodes that have been generated and add the start node to the set
+    generated = set()
+    generated.add(currentNode)
 
     # Create a list to store the nodes that have been visited
     visited = []
@@ -76,12 +76,12 @@ def RandomBFS(start: Node, goal: Node, costFunction: Callable):
         # Shuffle the children nodes to make the search random
         shuffle(children)
 
-        # For each child node, if the child node is the goal node, return the child node. Otherwise, add the child node to the queue and to the reached set
+        # For each child node, if the child node is the goal node, return the child node. Otherwise, add the child node to the queue and to the generated set
         for child in children:
             if child == goal:
-                return (child, len(reached)+1, len(visited)) # Return the goal node, the number of nodes reached (generated), and the number of nodes visited
-            if child not in reached:
-                reached.add(child)
+                return (child, len(generated)+1, len(visited)) # Return the goal node, the number of nodes generated (generated), and the number of nodes visited
+            if child not in generated:
+                generated.add(child)
                 frontier.put(child)
     
     # If the goal node is not found, return None
@@ -100,9 +100,9 @@ def DFS(start: Node, goal: Node, costFunction: Callable):
     frontier = []
     frontier.append(currentNode)
 
-    # Create a set to store the nodes that have been reached and add the start node to the set
-    reached = set()
-    reached.add(currentNode)
+    # Create a set to store the nodes that have been generated and add the start node to the set
+    generated = set()
+    generated.add(currentNode)
 
     # Create a list to store the nodes that have been visited
     visited = []
@@ -116,12 +116,12 @@ def DFS(start: Node, goal: Node, costFunction: Callable):
         # Expand the current node
         children = currentNode.expand(costFunction)
 
-        # For each child node, if the child node is the goal node, return the child node. Otherwise, add the child node to the stack and to the reached set
+        # For each child node, if the child node is the goal node, return the child node. Otherwise, add the child node to the stack and to the generated set
         for child in children:
             if child == goal:
-                return (child, len(reached)+1, len(visited)) # Return the goal node, the number of nodes reached (generated), and the number of nodes visited
-            if child not in reached:
-                reached.add(child)
+                return (child, len(generated)+1, len(visited)) # Return the goal node, the number of nodes generated (generated), and the number of nodes visited
+            if child not in generated:
+                generated.add(child)
                 frontier.append(child)
     
     # If the goal node is not found, return None
@@ -140,9 +140,9 @@ def RandomDFS(start: Node, goal: Node, costFunction: Callable):
     frontier = []
     frontier.append(currentNode)
 
-    # Create a set to store the nodes that have been reached and add the start node to the set
-    reached = set()
-    reached.add(currentNode)
+    # Create a set to store the nodes that have been generated and add the start node to the set
+    generated = set()
+    generated.add(currentNode)
 
     # Create a list to store the nodes that have been visited
     visited = []
@@ -159,12 +159,12 @@ def RandomDFS(start: Node, goal: Node, costFunction: Callable):
         # Shuffle the children nodes to make the search random
         shuffle(children)
 
-        # For each child node, if the child node is the goal node, return the child node. Otherwise, add the child node to the stack and to the reached set
+        # For each child node, if the child node is the goal node, return the child node. Otherwise, add the child node to the stack and to the generated set
         for child in children:
             if child == goal:
-                return (child, len(reached)+1, len(visited)) # Return the goal node, the number of nodes reached (generated), and the number of nodes visited
-            if child not in reached:
-                reached.add(child)
+                return (child, len(generated)+1, len(visited)) # Return the goal node, the number of nodes generated (generated), and the number of nodes visited
+            if child not in generated:
+                generated.add(child)
                 frontier.append(child)
     
     # If the goal node is not found, return None
@@ -179,9 +179,9 @@ def Dijkstra(start: Node, goal: Node, costFunction: Callable):
     frontier = PriorityQueue()
     frontier.put((0, id(current), current))
 
-    # Create a dictionary to store the nodes that have been reached and add the start node to the dictionary
-    reached = {}
-    reached[current] = current
+    # Create a dictionary to store the nodes that have been generated and add the start node to the dictionary
+    generated = {}
+    generated[current] = current
 
     # Create a list to store the nodes that have been visited
     visited = []
@@ -192,17 +192,17 @@ def Dijkstra(start: Node, goal: Node, costFunction: Callable):
         current = frontier.get()[2]
         visited.append(current)
         
-        # If the current node is the goal node, return the current node, the number of nodes reached (generated), and the number of nodes visited
+        # If the current node is the goal node, return the current node, the number of nodes generated (generated), and the number of nodes visited
         if current == goal:
-            return (current, len(reached), len(visited))
+            return (current, len(generated), len(visited))
         
         # Expand the current node
         children = current.expand(costFunction)
 
-        # For each child node, if the child node is not in the reached dictionary or the cost of the child node is less than the cost of the child node in the reached dictionary, add the child node to the reached dictionary and to the queue (Relaxation)
+        # For each child node, if the child node is not in the generated dictionary or the cost of the child node is less than the cost of the child node in the generated dictionary, add the child node to the generated dictionary and to the queue (Relaxation)
         for child in children:
-            if child not in reached or child.cost < reached[child].cost:
-                reached[child] = child
+            if child not in generated or child.cost < generated[child].cost:
+                generated[child] = child
                 frontier.put((child.cost, id(child), child))
     
     # If the goal node is not found, return None
@@ -216,9 +216,9 @@ def GBFS(start: Node, goal: Node, costFunction: Callable, heuristic: Callable):
     frontier = PriorityQueue()
     frontier.put((0, id(current), current))  # Priority = f(n) = h(n)
 
-    # Create a dictionary to store the nodes that have been reached and add the start node to the dictionary
-    reached = {}
-    reached[current] = current
+    # Create a dictionary to store the nodes that have been generated and add the start node to the dictionary
+    generated = {}
+    generated[current] = current
 
     # List of visited nodes
     visited = []
@@ -231,7 +231,7 @@ def GBFS(start: Node, goal: Node, costFunction: Callable, heuristic: Callable):
 
         # Check if it is the goal node
         if current == goal:
-            return current, len(reached), len(visited)
+            return current, len(generated), len(visited)
 
         # Expand the child nodes
         for child in current.expand(costFunction):
@@ -239,8 +239,8 @@ def GBFS(start: Node, goal: Node, costFunction: Callable, heuristic: Callable):
             f_cost = heuristic(child, goal)  # f(n) = h(n)
 
             # Check if the node should be updated
-            if child not in reached:
-                reached[child] = child  # Update the node in the reached dictionary
+            if child not in generated:
+                generated[child] = child  # Update the node in the generated dictionary
                 frontier.put((f_cost, id(child), child))  # Add to the priority queue
 
     # If the goal is not found, return None
@@ -255,9 +255,9 @@ def AStar(start: Node, goal: Node, costFunction: Callable, heuristic: Callable):
     frontier = PriorityQueue()
     frontier.put((0, id(current), current))  # Priority = f(n) = g(n) + h(n)
 
-    # Create a dictionary to store the nodes that have been reached and add the start node to the dictionary
-    reached = {}
-    reached[current] = current
+    # Create a dictionary to store the nodes that have been generated and add the start node to the dictionary
+    generated = {}
+    generated[current] = current
 
     # List of visited nodes
     visited = []
@@ -270,7 +270,7 @@ def AStar(start: Node, goal: Node, costFunction: Callable, heuristic: Callable):
 
         # Check if it is the goal node
         if current == goal:
-            return current, len(reached), len(visited)
+            return current, len(generated), len(visited)
 
         # Expand the child nodes
         for child in current.expand(costFunction):
@@ -279,8 +279,8 @@ def AStar(start: Node, goal: Node, costFunction: Callable, heuristic: Callable):
             f_cost = g_cost + heuristic(child, goal)  # f(n) = g(n) + h(n)
 
             # Check if the node should be updated
-            if child not in reached or g_cost < reached[child].cost:
-                reached[child] = child  # Update the node in the reached dictionary
+            if child not in generated or g_cost < generated[child].cost:
+                generated[child] = child  # Update the node in the generated dictionary
                 frontier.put((f_cost, id(child), child))  # Add to the priority queue
 
     # If the goal is not found, return None
